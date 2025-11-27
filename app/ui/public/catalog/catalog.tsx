ff68c2c6b02ee64b2a9d8de166b06c2474813de7
sseezov <sseezov@mail.ru>
2025-11-27 14:38:14 +0700

    исправил ошибку эффекта

--- app/ui/public/catalog/catalog.tsx
+++ app/ui/public/catalog/catalog.tsx
@@ -1,32 +1,22 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { Product, Category } from '../../../lib/definitions';
 import CategoriesSidebar from './categories-sidebar';
 import ProductsGrid from './products-grid';
 import styles from './catalog.module.css';
 
-interface CatalogProps {
+interface CatalogClientProps {
   products: Product[];
   categories: Category[];
 }
 
-export default function Catalog({ products, categories }: CatalogProps) {
+export default function CatalogClient({ products, categories }: CatalogClientProps) {
   const searchParams = useSearchParams();
   const urlCategory = searchParams.get('category');
-  
-  const [activeCategory, setActiveCategory] = useState<string>('all');
 
-  // При изменении параметра URL обновляем активную категорию
-  useEffect(() => {
-    if (urlCategory) {
-      console.log(1,urlCategory);
-      setActiveCategory(urlCategory);
-    }
-  }, [urlCategory]);
-
-  console.log(7, products, activeCategory);
+  const [activeCategory, setActiveCategory] = useState<string>(urlCategory || 'all');
 
   const filteredProducts = activeCategory === 'all'
     ? products
